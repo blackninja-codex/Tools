646ba27d12c14f96da40df543617df55a6c95e32
blackninja-codex <81924516+blackninja-codex@users.noreply.github.com>
2023-09-18 15:24:54 +0530

    Add files via upload

--- port_scanner.py
+++ port_scanner.py
@@ -1,20 +1,29 @@
 import sys
 import socket
 from datetime import datetime
+
 if len(sys.argv) == 2:
-	target = socket.gethostbyname(sys.argv[1])
+    target = socket.gethostbyname(sys.argv[1])
 else:
-	print("Invalid amount of Argument")
+    print("Invalid amount of Argument")
+
 print("Scanning Target: " + target)
 print("Scanning started at:" + str(datetime.now()))
+
 try:
-	for port in range(1,65535):
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		socket.setdefaulttimeout(1)
-		result = s.connect_ex((target,port))
-		if result ==0:
-			print("Port {} is open".format(port))
-		s.close()
-except Exception:
-		print("\nError!Exiting Program!")
-		sys.exit()
+  for port in range(1,65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target,port))
+        if result == 0:
+            print("Port {} is open".format(port))
+        s.close()
+except KeyboardInterrupt:
+        print("\n Exiting Program !!!!")
+        sys.exit()
+except socket.gaierror:
+        print("\n Hostname Could Not Be Resolved !!!!")
+        sys.exit()
+except socket.error:
+        print("\ Server not responding !!!!")
+        sys.exit()
